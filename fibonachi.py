cfe6c7fc6658d617bc2e24fe0cdf873efd07d342
vlykh <vlykh@softserveinc.com>
2017-12-04 18:35:16 +0200

    4

--- fibonachi.py
+++ fibonachi.py
@@ -2,16 +2,16 @@ def fib(n):
     s0 = 0
     s1 = 1
     if n>=1:
-        print (s0)
+        print (s0),
     if n>=2:
-        print (s1)
+        print (s1),
     if n>=3:
         for i in range(2, n):
             s2 = s0 + s1
             s0=s1
             s1=s2
-            print (s2)
+            print (s2),
 
 
-n = 2
+n = 7
 fib(n)
