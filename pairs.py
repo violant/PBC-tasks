5d696cc55f7acffbbd47d1dca06ae47d1fc23169
vlykh <vlykh@softserveinc.com>
2017-12-05 11:11:28 +0200

    add uniqueness check for pairs and implement result as set

--- pairs.py
+++ pairs.py
@@ -4,13 +4,15 @@ summ = 10
 def find_pair(cur, *list_elem):
     for ele in list_elem:
         if ele + cur == summ:
-            return ele, cur
+            return min(ele, cur), max(ele, cur)
 
 
 def pair(list_elem):
+    pair_res = set()
     for elem in list_elem:
-        print(find_pair(elem, *list_elem))
+        pair_res.add(find_pair(elem, *list_elem))
+    return pair_res
 
 
 list_ele = [1, 2, 3, 4, 5, 6, 7, 8, 9, 8]
-pair(list_ele)
+print(pair(list_ele))
