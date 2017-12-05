5ed95f9e56e09241a67e05a3ceb4db60e8de3f6b
vlykh <vlykh@softserveinc.com>
2017-12-05 14:17:24 +0200

    fix None as element in set

--- pairs.py
+++ pairs.py
@@ -4,16 +4,16 @@ summ = 10
 def find_pair(cur, *list_elem):
     for ele in list_elem:
         if ele + cur == summ:
-            tup=(ele, cur)
             return min(ele, cur), max(ele, cur)
 
 
 def pair(list_elem):
     pair_res = set()
     for elem in list_elem:
-        pair_res.add(find_pair(elem, *list_elem))
+        if find_pair(elem, *list_elem) is not None:
+            pair_res.add(find_pair(elem, *list_elem))
     return pair_res
 
 
-list_ele = [1, 2, 3, 4, 5, 6, 7, 8, 9, 8]
+list_ele = [17, 4, 5, 6, 5, 4, 9]
 print(pair(list_ele))
