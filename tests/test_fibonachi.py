2383dcb4f665a94bed4a5d3ec2adb1ed8b0a1ef6
vlykh <vlykh@softserveinc.com>
2017-12-07 17:01:28 +0200

    add parametrize

--- tests/test_fibonachi.py
+++ tests/test_fibonachi.py
@@ -1,23 +1,28 @@
+# content of test_expectation.py
+import pytest
 from apps.fibonachi import fib
+
 arr_pass = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711]
 
 
-def test_count():
-    for i in range(10):
-        assert fib(i).__len__() == i
+@pytest.mark.parametrize("test_input,expected", [
+    (1, arr_pass[0:1]),
+    (10, arr_pass[0:10]),
+    (20, arr_pass[0:20])
+])
+def test_list(test_input, expected):
+    assert fib(test_input) == expected
 
 
-def test_list():
-    for i in range(10):
-        assert fib(i) == arr_pass[0:i]
+@pytest.mark.parametrize("test_input,expected", [
+    (1, 1),
+    (10, 10),
+    (20, 20)
+])
+def test_count(test_input, expected):
+    assert fib(test_input).__len__() == expected
 
 
-def test_neg_1():
-    assert fib(45.8).__len__() == 0
-
-
-def test_neg_2():
-    assert fib('str').__len__() == 0
 
 
 
