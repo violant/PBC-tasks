a06a7dd651f676c50ad7feb3284a2082c51c5b86
vlykh <vlykh@softserveinc.com>
2017-12-08 13:10:23 +0200

    add pytest.mark

--- tests/test_fibonachi.py
+++ tests/test_fibonachi.py
@@ -5,6 +5,7 @@ from apps.fibonachi import fib
 arr_pass = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711]
 
 
+@pytest.mark.sequence
 @pytest.mark.parametrize("test_input,expected", [
     (1, arr_pass[0:1]),
     (10, arr_pass[0:10]),
@@ -14,6 +15,7 @@ def test_list(test_input, expected):
     assert fib(test_input) == expected
 
 
+@pytest.mark.count
 @pytest.mark.parametrize("test_input,expected", [
     (1, 1),
     (10, 10),
@@ -21,12 +23,3 @@ def test_list(test_input, expected):
 ])
 def test_count(test_input, expected):
     assert fib(test_input).__len__() == expected
-
-
-
-
-
-
-
-
-
