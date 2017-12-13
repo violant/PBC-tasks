270d382f2df9b11a54d7c5a7df86be29f597752b
vlykh <vlykh@softserveinc.com>
2017-12-13 11:16:33 +0200

    raise exception #7

--- pbc/sg/test_sel_grid.py
+++ pbc/sg/test_sel_grid.py
@@ -1,4 +1,5 @@
 from pbc.sg.sg import StartGrid, Grid
+import selenium
 
 
 def test_sg_sm(ssh_client):
@@ -8,3 +9,18 @@ def test_sg_sm(ssh_client):
     grid.add_node()
     res = ssh_client.execute('pgrep java')
     assert len(res) == 2
+    g
+
+
+def test_sg_grid_firefox_icons(ssh_client):
+    grid = StartGrid(Grid(ssh_client))
+    grid.download()
+    grid.download_json()
+    grid.start_hub()
+    grid.add_node()
+    driver = webdriver.Firefox()
+    driver.get("http://192.168.33.10:4444/grid/console")
+    elem = driver.find_elements_by_xpath(
+        "//div[@type='browsers' and @class='content_detail']//img[contains(@title,'firefox')]")
+    assert 5, elem.count()
+    driver.close()
